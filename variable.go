11c7fa916033a93efc9594db06b91721768e4205
jihadanbs <jihadanbs11@gmail.com>
2024-11-19 03:58:54 +0700

    push function variable

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	name := "Jihadan"
+	fmt.Println(name)
+
+	name = "Jihadan Bs"
+	fmt.Println(name)
+
+	var (
+		namaDepan    = "Jihadan"
+		namaBelakang = "Beckhiano"
+	)
+
+	fmt.Println(namaDepan, namaBelakang)
+	fmt.Println(namaBelakang)
+}
